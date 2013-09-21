0817f933d10b0ffc530570a61c4df2320d82fd46
swapnilm <swapnilm@flipkart.com>
2013-09-21 22:15:01 +0530

    added sign up button and page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  root :to => 'static_pages#home'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/about', to: 'static_pages#about'
+  match '/signup',  to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -55,7 +56,6 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
